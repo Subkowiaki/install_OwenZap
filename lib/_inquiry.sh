07a0e974070c617d725ceeac877b4110e6eb4eca
Marcelo Subkowiaki JUnior <55325702+Subkowiaki@users.noreply.github.com>
2022-06-17 12:53:27 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -8,14 +8,6 @@ get_mysql_root_password() {
   read -p "> " mysql_root_password
 }
 
-get_link_git() {
-  
-  print_banner
-  printf "${WHITE} 💻 Insira o link do GITHUB do sistema:${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " link_git
-}
-
 get_instancia_add() {
   
   print_banner
